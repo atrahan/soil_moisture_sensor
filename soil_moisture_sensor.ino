74bb5f7244f75ce2bff1224ef7dd925f00fdafad
atrahan <alextrahan@gmail.com>
2019-09-04 12:49:59 -0700

    Add light sensor, led, and buzzer functionality

--- soil_moisture_sensor.ino
+++ soil_moisture_sensor.ino
@@ -1,30 +1,64 @@
-/* 
- Soil Moisture Sensor  
+/*
+ Soil Moisture Sensor
  Trahan, A
  04 Sep 19
 */
 
 // Pin Definitions
 #define SensorPin A0
+#define PhotoResPin A1
+#define BuzzzerPin 8
+#define LedPin 7
 
 // Variable Definitions
-float sensorValue = 0;
-float dryValue = 600;
-float wetValue = 400;
+float sensorValue = 0.0;
+float photoValue = 0.0;
+float ledThresh = 475;
+float buzzThresh = 550;
+int t = 0;
+int dt = 1e3;
 
 // Initialize
-void setup() { 
- Serial.begin(9600); 
-} 
+void setup() {
+ Serial.begin(9600);
+ pinMode(LedPin, OUTPUT);
+}
 
 // Run
-void loop() { 
- for (int i = 0; i <= 100; i++) 
+void loop() {
+ // Take average of 50 sensor readings for soil moisture and photo
+ for (int i = 0; i <= 50; i++)
  { 
-   sensorValue = sensorValue + analogRead(SensorPin); 
-   delay(1); 
- } 
- sensorValue = sensorValue/100.0; 
- Serial.println(sensorValue); 
- delay(30); 
-} 
+   sensorValue = sensorValue + analogRead(SensorPin);
+   photoValue = photoValue + analogRead(PhotoResPin);
+   delay(1);
+ }
+ sensorValue = sensorValue/50.0;
+ photoValue = photoValue/50.0;
+
+  // If very dry, blink (always) and buzz (slowly, and only if lights are on)
+  if (sensorValue > buzzThresh) {
+    if (photoValue > 60 && t>=1e4) {
+      tone(BuzzzerPin, 440, 50);
+      t = 0;
+    }
+    digitalWrite(LedPin, !digitalRead(LedPin));
+  }
+  // If somewhat dry, turn on LED
+  else if (sensorValue > ledThresh) {
+    digitalWrite(LedPin, 1);
+  }
+  // If wet enough, turn off LED
+  else {
+    digitalWrite(LedPin, 0);
+  }
+
+ // Print values to console for debugging
+ Serial.print(sensorValue);
+ Serial.print("\t");
+ Serial.println(photoValue);
+
+ // Increment timer for slow buzzer
+ t = t + dt;
+ delay(dt);
+}
